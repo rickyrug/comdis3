f320ecaea80b7cd02bf96a3b64fe052a01ad9c5f
60044723 <60044723@TAMP00033244.tenaris.techint.net>
2017-01-05 17:32:22 -0600

    modulo de agregar venta detalle

--- application/controllers/SellsDetail.php
+++ application/controllers/SellsDetail.php
@@ -13,11 +13,34 @@
  */
 require_once( APPPATH.'/interfaces/ICrud.php' );
 class SellsDetail extends MY_Controller implements ICrud{
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Client_Model');
+        $this->load->model('SellHdr_Model');
+        $this->load->helper('dropdown');
+        $clientsList = $this->Client_Model->find_all_entries();
+        $this->smarty->assign('client_options',generateClientsDropdownInformation($clientsList));
+        $this->smarty->assign('type_options',  $this->setSellsTypes());
+    }
+    
     public function addConfirmation() {
         
     }
 
      public function addViewDetail($p_id) {
+        
+         $sell = $this->SellHdr_Model->find_by_key($p_id);
+        
+        if(count($sell) > 0){
+           $this->data['sell'] = $sell[0];
+           
+           $this->smarty->assign('customer_id',  $sell[0]->idclient);
+           $this->smarty->assign('typeid',  $sell[0]->type);
+           $this->CallViews('SellsDetail/addform.tpl', $this->data);
+        }else{
+            redirect('ProductPriceClient');
+        }
         echo $p_id;
     }
     
@@ -44,5 +67,11 @@ class SellsDetail extends MY_Controller implements ICrud{
         
     }
 
+    private function setSellsTypes() {
+        $finalarray = array();
+
+        $finalarray['RM'] = $this->lang->line('remission');
+        return $finalarray;
+    }
 //put your code here
 }
