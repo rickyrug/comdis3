58890476cc05f426085e012f00a6d363d73c4cd1
60044723 <60044723@TAMP00033244.tenaris.techint.net>
2017-01-06 17:20:39 -0600

    modulo de agregar venta detalle && logic

--- application/controllers/SellsDetail.php
+++ application/controllers/SellsDetail.php
@@ -14,18 +14,24 @@
 require_once( APPPATH.'/interfaces/ICrud.php' );
 class SellsDetail extends MY_Controller implements ICrud{
     
+    private $user;
+    
     public function __construct() {
         parent::__construct();
         $this->load->model('Client_Model');
         $this->load->model('SellHdr_Model');
         $this->load->model('Product_Model');
         $this->load->model('ClientPrices_Model');
+        $this->load->model('SellDtl_Model');
         $this->load->helper('dropdown');
+        
         $clientsList = $this->Client_Model->find_all_entries();
         $productList = $this->Product_Model->find_all_entries();
         $this->smarty->assign('client_options',generateClientsDropdownInformation($clientsList));
         $this->smarty->assign('type_options',  $this->setSellsTypes());
         $this->smarty->assign('product_options',  generateProductDropdownInformation($productList));
+        
+        $this->user = 1;
     }
     
     public function addConfirmation() {
@@ -33,12 +39,21 @@ class SellsDetail extends MY_Controller implements ICrud{
     }
 
      public function addProductConfirmation() {
-        $p_idsell = $_POST['idsells'];
+        $p_idsell = $_POST['idsell'];
         $p_idproduct = $_POST['idproduct'];
         $p_quantity = $_POST['quantity'];
-        $p_price = $_POST['quantity'];
+        $p_price = $_POST['price'];
         $p_discount = $_POST['discount'];
-       
+        $p_tax      = $this->getSpecialProductTaxes($p_idproduct);
+        $result = $this->SellDtl_Model->insert_entry( $p_idsell,$p_idproduct, $p_tax,
+                                    $p_discount,$p_price,$p_quantity,$this->user);
+        
+        $confirm_msg ="";
+        if($result['err']['code']=== 0){
+            $confirm_msg = sprintf($this->lang->line('addconfirm_registy'),$p_idsell);
+        }
+        $result['confirm_msg'] = $confirm_msg;
+        echo json_encode($result);
     }
     
      public function addViewDetail($p_id) {
@@ -78,7 +93,31 @@ class SellsDetail extends MY_Controller implements ICrud{
     }
 
     public function updateView($p_id) {
+    // echo $smarty->fetch('SellsDetail/editdtlitem.tpl');
+    }
+     public function updateViewItem() {
+      $idsellsdtl =   $_GET['idsells_dtl'];
+      $product    =   $_GET['name']; //product name
+      $quantity    =   $_GET['quantity']; //quantity
+      $price   =   $_GET['price']; //quantity
+      
+      $this->smarty->assign('idselldtl',$idsellsdtl);
+      $this->smarty->assign('idproduct',$product );
+      $this->smarty->assign('quantity',$quantity );
+      $this->smarty->assign('price',$price);
+     echo $this->smarty->fetch('SellsDetail/editdtlitem.tpl');
+    }
+    public function getSellsItems(){
+       
+       $result = array();
+       $p_idsells = $_POST['idsell'];
         
+       $result['items'] = $this->SellDtl_Model->find_by_sells($p_idsells);
+       
+       $sell = $this->SellHdr_Model->find_by_key($p_idsells);
+       
+       $result['summary'] = $this->sellssummary($result['items'],$sell[0]);
+       echo json_encode($result);
     }
     
     public function getProductRecomendedPrice(){
@@ -111,5 +150,32 @@ class SellsDetail extends MY_Controller implements ICrud{
         $finalarray['RM'] = $this->lang->line('remission');
         return $finalarray;
     }
-//put your code here
+    
+    private function getSpecialProductTaxes($p_idproduct){
+        return 0.0;
+    }
+    
+    private function sellssummary($sellsdata,$sell){ // se calcula el resumen
+        
+        setlocale(LC_MONETARY, 'en_US.UTF-8');
+        
+        $summary = array();
+        $undertotal = 0.0;
+        $taxper      = $sell->tax;
+        $discountper =  $sell->discount;
+        $taxtotal = 0.0;
+        $discount = 0.0;
+        $total = 0.0;
+        foreach($sellsdata as $item){
+            $undertotal = $undertotal + ($item->quantity * $item->price);
+        }
+        $summary['undertotal'] = '$'.number_format($undertotal,2);
+        $discount = $undertotal * $discountper; 
+        $summary['discount']   = '$'.number_format($discount, 2); 
+        $taxtotal = ($undertotal - $discount) * $taxper;
+        $summary['tax']        =  '$'.number_format($taxtotal,2);
+        $total = $undertotal -$discount - $taxtotal;
+        $summary['total'] = '$'.number_format($total,2);
+        return  $summary;
+    }
 }
